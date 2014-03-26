355513a2c0f13340309c4bbd83902e9786e50098
smileisak <ikaboubi@gmail.com>
2014-03-26 21:53:59 +0100

    SplashScreen fixed final

---
+++ src/com/example/leetfinder/ScanActivity.java
@@ -0,0 +1,15 @@
+package com.example.leetfinder;
+
+import com.example.leetfinder.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScanActivity extends Activity{
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_scan);
+	}
+}
