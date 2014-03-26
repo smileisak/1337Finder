977f557a1e9f05ea5e60ebeb1a15ea099bfe5d90
smileisak <ikaboubi@gmail.com>
2014-03-26 22:23:52 +0100

    First Merge

--- gen/com/example/leetfinder/R.java
+++ gen/com/example/leetfinder/R.java
@@ -985,6 +985,12 @@ containing a value of this type.
         public static final int wel=0x7f020058;
     }
     public static final class id {
+        public static final int BSSID=0x7f05003f;
+        public static final int BSSID_Value=0x7f050043;
+        public static final int ESSID=0x7f050041;
+        public static final int ESSID_Value=0x7f050044;
+        public static final int IP=0x7f050042;
+        public static final int IP_Value=0x7f050045;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -996,12 +1002,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int connect_btn=0x7f050040;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1150,7 +1157,11 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int boutton_name=0x7f0a0011;
+        public static final int bssid=0x7f0a0012;
+        public static final int essid=0x7f0a0010;
         public static final int hello_world=0x7f0a000e;
+        public static final int ip=0x7f0a0013;
     }
     public static final class style {
         /** 
--- src/com/example/leetfinder/ScanActivity.java
+++ src/com/example/leetfinder/ScanActivity.java
@@ -1,15 +1,90 @@
 package com.example.leetfinder;
 
-import com.example.leetfinder.R;
 
 import android.app.Activity;
+import android.content.*;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
 
-public class ScanActivity extends Activity{
+
+
+public class ScanActivity extends Activity {
+	
+	private WifiManager wifiManager;
+	private Button btn;
+	private WifiInfo info=null; 
+	private String bssid ;
+	private String essid ;
+	private String ip ;
+	
+	
+ public String getIpAddr() {
+		   WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+		   WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		   int ip = wifiInfo.getIpAddress();
+
+		   String ipString = String.format(
+		   "%d.%d.%d.%d",
+		   (ip & 0xff),
+		   (ip >> 8 & 0xff),
+		   (ip >> 16 & 0xff),
+		   (ip >> 24 & 0xff));
+
+		   return ipString;
+		}	
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_scan);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scan);
+		
+		wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);
+		info = wifiManager.getConnectionInfo();
+		
+		bssid = info.getBSSID();
+		TextView bssidTextView = (TextView) findViewById(R.id.BSSID_Value);
+        bssidTextView.setText(bssid);
+        
+        essid = info.getSSID();
+		TextView essidTextView = (TextView) findViewById(R.id.ESSID_Value);
+        essidTextView.setText(essid);
+        
+        ip = getIpAddr();
+		TextView ipTextView = (TextView) findViewById(R.id.IP_Value);
+        ipTextView.setText(ip);
+
+        
+        btn = (Button) findViewById(R.id.connect_btn);
+        if (bssid == null)
+        	Toast.makeText(ScanActivity.this, "Press connect Button...", Toast.LENGTH_LONG).show();
+        
+        btn.setOnClickListener(new OnClickListener(){
+            public void onClick(View v) {
+            // TODO Auto-generated method stub
+            	 if (bssid != null)
+            		 Toast.makeText(ScanActivity.this, "You have allready connected to " + essid, Toast.LENGTH_LONG).show();
+            	 else
+            		 {Toast.makeText(ScanActivity.this, "Need Wifi Connection", Toast.LENGTH_LONG).show();
+            	     startActivity(new Intent(WifiManager.ACTION_PICK_WIFI_NETWORK));}
+            	 	 
+            }
+
+        });
+
+    /*    if (info.getBSSID() != null) {
+        	 //  int force = WifiManager.calculateSignalLevel(info.getRssi(), 5);
+        	   Toast.makeText(this, "Connecté à " + info.getSSID() +
+        	   " à " + info.getLinkSpeed() + WifiInfo.LINK_SPEED_UNITS +
+        	   " avec une force " + "/5   " +getIpAddr() , 1000000000).show();
+        	}       
+        
+        
+	}*/
+
 	}
+
 }
